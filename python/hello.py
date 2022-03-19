225749fd797eec63374679d99978aaa1ccf87c33
germancin <elmaildegerman@gmail.com>
2022-03-19 15:24:27 -0400

    update

--- python/hello.py
+++ python/hello.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+print("Script started!")
 #GPIO SETUP
 channel = 17
 GPIO.setmode(GPIO.BCM)
@@ -11,6 +12,7 @@ def callback(channel):
 			print("Sound Detected!")
 	else:
 			print("Sound Detected!")
+    
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
@@ -18,3 +20,4 @@ GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run f
 # infinite loop
 while True:
         time.sleep(1)
+        print("!")
