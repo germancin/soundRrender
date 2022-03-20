940d0167159518cf5c84c7918f01f0a949890353
germancin <elmaildegerman@gmail.com>
2022-03-20 09:30:23 -0400

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
+import Grid from 'material-grid/dist/Grid/Grid';
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ count,setCount ] = useState( 0 )
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ logo } className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <button type="button" onClick={ () => setCount( ( count ) => count + 1 ) }>
+            count is: { count }
           </button>
         </p>
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
+        <Grid>
+
+          <Grid style={ { border: "2px solid yellow" } }>
+            <button type="button" onClick={ () => console.log( "less" ) }>
+              -
+            </button>
+            <button type="button" onClick={ () => console.log( "more" ) }>
+              +
+            </button>
+          </Grid>
+
+
+          { ' | ' }
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -36,7 +40,7 @@ function App() {
           >
             Vite Docs
           </a>
-        </p>
+        </Grid>
       </header>
     </div>
   )
