66961aea0419d2bcd224ee0c26cdcfd4fca58c21
germancin <elmaildegerman@gmail.com>
2022-03-20 12:24:50 -0400

    update

--- server.js
+++ server.js
@@ -191,6 +191,10 @@ server.get("/playmp3", (req, res) => {
 
 
 
+
+
+
+
 let streamArray = [];
 let toleranceVal = 0;
 let sounded = false;
@@ -319,11 +323,6 @@ server.post("/updateVolumneTreshold", (req, res) => {
 
 server.post("/updateTolerance", (req, res) => {
 	try {
-		let streamArray = [];
-		let toleranceVal = 0;
-		let sounded = false;
-		let toleranTimerOn = false;
-
 		const toleranceValueRequest = req.body.tolerance || null;
 
 		console.log("REQUEST:::::", req.body.tolerance || false);
@@ -332,108 +331,8 @@ server.post("/updateTolerance", (req, res) => {
 			toleranceLoud = toleranceValueRequest;
 		}
 
-		const handleSoundStream = (stream) => {
-			streamArray.push(stream);
-			console.log(streamArray.length);
-		};
-
-		const readInput = (err) => {
-			try {
-				console.log("Listening...");
-
-				if (err) {
-					console.log("Error listening", err);
-					// res.status(500).json({
-					// 	ping: channel,
-					// 	message: err,
-					// });
-				}
-
-				gpio.on("change", function (channel, changedValue) {
-					gpio.read(channel, function (err, value) {
-						if (err) {
-							console.log("Error event", err);
-							// res.status(500).json({
-							// 	ping: channel,
-							// 	message: err.message,
-							// });
-						}
-
-						changedValue && value && handleSoundStream(value);
-					});
-				});
-
-				// res.status(200).json({
-				// 	ping: channel,
-				// 	messge: "The sensor started listening succesfully.",
-				// });
-			} catch (error) {
-				console.log("error", error);
-				// res.status(500).json({
-				// 	ping: channel,
-				// 	message: err.message,
-				// });
-			}
-		};
-
-		const tolerance = (value) => {
-			toleranceVal = toleranceVal + value;
-
-			console.log("WHAT DO WEHAVE", volumeThreshold);
-			if (toleranceVal > volumeThreshold && !sounded) {
-				const python = spawn("python", ["python/playmp3.py"]);
-				console.log(
-					"::::::::::::::::::::::::::::::::::YOU ARE TALKING TOO HIGH !!!!",
-					toleranceVal
-				);
-				sounded = true;
-				toleranceVal = 0;
-				startCounter();
-			} else {
-				console.log("Ydoundede false??", sounded);
-			}
-		};
-
-		setInterval(function () {
-			if (streamArray.length > toleranceLoud) {
-				console.log("::::::GETTING  LOUGHT UPDATE", streamArray.length);
-				tolerance(streamArray.length);
-				!toleranTimerOn && startToleranceTimer();
-			}
-
-			streamArray = [];
-		}, 3000);
-
-		const startToleranceTimer = () => {
-			let counter = 10;
-			toleranTimerOn = true;
-			let interval = setInterval(function () {
-				counter = counter - 1;
-
-				console.log("tolerance", counter, toleranceVal);
-
-				if (counter === 0) {
-					clearInterval(interval);
-					toleranceVal = 0;
-					toleranTimerOn = false;
-				}
-			}, 1000);
-		};
-
-		const startCounter = () => {
-			let counter = 20;
-			let interval = setInterval(function () {
-				counter = counter - 1;
-				console.log(" ::: can sound in", counter);
-				if (counter === 0) {
-					clearInterval(interval);
-					sounded = false;
-					toleranceVal = 0;
-				}
-			}, 1000);
-		};
-
 		gpio.setup(channel, gpio.DIR_IN, gpio.EDGE_BOTH, readInput);
+
 	} catch (error) {
 		res.status(500).json({
 			error: error.message,
