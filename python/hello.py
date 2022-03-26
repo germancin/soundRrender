eed1fd0bd5ae3fa6377574adbd29c6a56b9f051b
germancin <elmaildegerman@gmail.com>
2022-03-25 21:36:43 -0400

    update

--- python/hello.py
+++ python/hello.py
@@ -2,23 +2,25 @@ import RPi.GPIO as GPIO
 import time
 
 print("Script started!")
-#GPIO SETUP
+# GPIO SETUP
 channel = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
-def callback(channel):
-	if GPIO.input(channel):
-			print("Sound Detected!")
-	else:
-			print("Sound Detected!")
-    
 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+def callback(channel):
+    if GPIO.input(channel):
+        print("Sound Detected!", channel)
+    else:
+        print("Sound Detected!")
+
+
+# let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+# assign function to GPIO PIN, Run function on change
+GPIO.add_event_callback(channel, callback)
 print("Script ended!")
 
 # infinite loop
 while True:
-        time.sleep(1)
-        print(".")
+    time.sleep(1)
