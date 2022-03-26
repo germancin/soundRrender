53c79bc383f0b6f3ce849f821783a11113a44594
germancin <elmaildegerman@gmail.com>
2022-03-25 21:06:05 -0400

    update

--- python/core/blink_led/blink_led.py
+++ python/core/blink_led/blink_led.py
@@ -8,10 +8,13 @@ GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 GPIO.setup(ledPin, GPIO.OUT)   # Set ledPin's mode as output
 GPIO.output(ledPin, GPIO.HIGH)  # Set ledPin high(+3.3V) to turn on led
 
-while True:  # Continue looping indefinitely
+counter = 0
+
+while counter < 10:  # Continue looping indefinitely
     GPIO.output(ledPin, GPIO.HIGH)  # Turn led on
     time.sleep(1)  # Pause for 1 second
     GPIO.output(ledPin, GPIO.LOW)  # Turn led off
     time.sleep(1)  # Pause for 1 second
+    counter = counter + 1
 
 GPIO.cleanup()  # Clean up when exiting the program
